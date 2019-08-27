62a74eae43782a8b2821bd4b3c3f96d8b111ed1a
Jack <durcan.j@gmail.com>
2019-08-27 10:44:35 +0100

    Add TryGetOrDefault extension for dictionaries

---
+++ src/Gibe.Collections/Gibe.Collections/Extensions/DictionaryExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace Gibe.Collections.Extensions
+{
+	public static class DictionaryExtensions
+	{
+		public static T TryGetOrDefault<T>(this Dictionary<string, T> dictionary, string key)
+		{
+			T value;
+			if (dictionary.TryGetValue(key, out value))
+			{
+				return value;
+			}
+
+			return default(T);
+		}
+
+		public static T TryGetOrDefault<T>(this Dictionary<string, T> dictionary, string key, T defaultValue)
+		{
+			var value = dictionary.TryGetOrDefault(key);
+
+			return value.Equals(default(T)) ? defaultValue : value;
+		}
+	}
+}
